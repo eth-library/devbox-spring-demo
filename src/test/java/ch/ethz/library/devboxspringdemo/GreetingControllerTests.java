4e9a7cff03f6964012be3db2cc79c612eba157f2
jcardozo-eth <jcardozo@ethz.ch>
2025-10-27 13:15:00 +0100

    test: migrate to RestTestClient

--- src/test/java/ch/ethz/library/devboxspringdemo/GreetingControllerTests.java
+++ src/test/java/ch/ethz/library/devboxspringdemo/GreetingControllerTests.java
@@ -1,37 +1,40 @@
 package ch.ethz.library.devboxspringdemo;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.Test;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.client.RestTestClient;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
-@AutoConfigureMockMvc
 public class GreetingControllerTests {
 
-    @Autowired
-    private MockMvc mockMvc;
+    RestTestClient client =
+            RestTestClient.bindToController(new GreetingController()).build();
 
     @Test
     public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
-
-        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$.content").value("Hello, World!"));
+        client.get().uri("/greeting")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Greeting.class)
+                .consumeWith(result -> {
+                    assertThat(result.getResponseBody().content())
+                        .isEqualTo("Hello, World!");
+                });
     }
 
     @Test
     public void paramGreetingShouldReturnTailoredMessage() throws Exception {
 
-        this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
-                .andDo(print()).andExpect(status().isOk())
-                .andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
+        client.get().uri("/greeting?name=Spring Community")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Greeting.class)
+                .consumeWith(result -> {
+                    assertThat(result.getResponseBody().content())
+                        .isEqualTo("Hello, Spring Community!");
+                });
     }
 
 }
